4f48e175dde952ebe71a0aec84db5a6edd45cb66
Sunil <sunil.salaria@lpinfotech.com>
2024-10-21 19:02:31 +0530

    change folder view

--- client/webUI/src/app/app.routes.ts
+++ client/webUI/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { EmployeeCodeComponent } from './pages/employee-code/employee-code.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { AdminLoginAccessComponent } from './pages/admin-login-access/admin-login-access.component';
-import { SelectGiftVoucherComponent } from './pages/select-gift-voucher/select-gift-voucher.component';
-import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { EmployeeCodeComponent } from './pages/auth/employee-code/employee-code.component';
+import { AdminLoginAccessComponent } from './pages/auth/admin-login-access/admin-login-access.component';
+import { SelectGiftVoucherComponent } from './pages/employee/select-gift-voucher/select-gift-voucher.component';
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './shared/home/home.component';
+import { PageNotFoundComponent } from './pages/error/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     //auth routes
-    { path: '', component: EmployeeCodeComponent },
+    { path: '', component: HomeComponent },
     { path: 'employee-code', component: EmployeeCodeComponent },  
     { path: 'admin-login-access', component: AdminLoginAccessComponent }, 
     { path: 'select-gift-voucher', component: SelectGiftVoucherComponent }, 
