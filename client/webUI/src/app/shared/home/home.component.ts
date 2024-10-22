99d21289080f96f38580ce508437df6a3da79a8e
Sunil <sunil.salaria@lpinfotech.com>
2024-10-22 11:16:57 +0530

    update landing page

--- client/webUI/src/app/shared/home/home.component.ts
+++ client/webUI/src/app/shared/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { EmployeeCodeComponent } from '../../pages/auth/employee-code/employee-code.component';
+import { AdminLoginAccessComponent } from '../../pages/auth/admin-login-access/admin-login-access.component';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EmployeeCodeComponent, AdminLoginAccessComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+constructor(private router : Router){}
 }
