bb2a149e54f6950c22bad694a75339487d7635a1
Sunil Salaria <sunil.salaria@lpinfotech.com>
2024-10-30 11:29:57 +0530

    Error page

--- client/webUI/src/app/pages/error/page-not-found/page-not-found.component.spec.ts
+++ client/webUI/src/app/pages/error/page-not-found/page-not-found.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 
+
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
--- client/webUI/src/app/pages/error/page-not-found/page-not-found.component.ts
+++ client/webUI/src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { Location } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
