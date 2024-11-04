0c9846471bed06c4554d7511f986f9094b6ff313
Sunil Salaria <sunil.salaria@lpinfotech.com>
2024-11-04 13:22:18 +0530

    error page

--- client/webUI/src/app/pages/error/page-not-found/page-not-found.component.ts
+++ client/webUI/src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { Location } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule,RouterLink],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
-  constructor(private location: Location) { }
+  constructor(private location: Location,private router: Router) { }
 //== go back btn
 goBack(): void {
   this.location.back();
