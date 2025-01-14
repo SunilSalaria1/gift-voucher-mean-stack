0545d1d1b086fe3e366070b76b8106ab68a166eb
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-14 15:39:05 +0530

    made gift section

--- client/webUI/src/app/pages/employee/select-gift-voucher/select-gift-voucher.component.ts
+++ client/webUI/src/app/pages/employee/select-gift-voucher/select-gift-voucher.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-select-gift-voucher',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, RouterLink,MatButtonModule,MatCardModule],
   templateUrl: './select-gift-voucher.component.html',
   styleUrl: './select-gift-voucher.component.css'
 })
