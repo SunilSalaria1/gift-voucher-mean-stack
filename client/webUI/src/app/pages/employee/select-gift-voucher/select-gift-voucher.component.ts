ef551860518715c624717379aa22e3e03375f44b
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-15 13:44:15 +0530

    added product folder in images

--- client/webUI/src/app/pages/employee/select-gift-voucher/select-gift-voucher.component.ts
+++ client/webUI/src/app/pages/employee/select-gift-voucher/select-gift-voucher.component.ts
@@ -14,16 +14,16 @@ import { RouterLink } from '@angular/router';
 export class SelectGiftVoucherComponent {
 
   productList: any[] = [
-    { id: 1, code: "CODE98547", image: "assets/products/water-bottle.png", name: "Copper bottle", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 2, code: "CODE98981", image: "assets/products/perfume.png", name: "Engage perfume", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 3, code: "CODE93457", image: "assets/products/ear-buds.png", name: "Ear buds", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 4, code: "CODE34780", image: "assets/products/chair.png", name: "Office chair", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 5, code: "CODE51547", image: "assets/products/tumbler.png", name: "Tumbler", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 6, code: "CODE78456", image: "assets/products/power-bank.png", name: "Power bank", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 7, code: "CODE90927", image: "assets/products/steel-water-bottle.png", name: "Cello water bottle", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 8, code: "CODE76047", image: "assets/products/laptop-screen.png", name: "Laptop screen", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 9, code: "CODE98264", image: "assets/products/bluetooth-speaker.png", name: "Bluetooth speaker", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
-    { id: 10, code: "CODE63154", image: "assets/products/lunch-box.png", name: "Lunch box", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 1, code: "CODE98547", image: "assets/images/products/water-bottle.png", name: "Copper bottle", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 2, code: "CODE98981", image: "assets/images/products/perfume.png", name: "Engage perfume", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 3, code: "CODE93457", image: "assets/images/products/ear-buds.png", name: "Ear buds", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 4, code: "CODE34780", image: "assets/images/products/chair.png", name: "Office chair", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 5, code: "CODE51547", image: "assets/images/products/tumbler.png", name: "Tumbler", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 6, code: "CODE78456", image: "assets/images/products/power-bank.png", name: "Power bank", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 7, code: "CODE90927", image: "assets/images/products/steel-water-bottle.png", name: "Cello water bottle", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 8, code: "CODE76047", image: "assets/images/products/laptop-screen.png", name: "Laptop screen", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 9, code: "CODE98264", image: "assets/images/products/bluetooth-speaker.png", name: "Bluetooth speaker", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
+    { id: 10, code: "CODE63154", image: "assets/images/products/lunch-box.png", name: "Lunch box", description: "This product is most popular and most rated by customers.", text: "Claim Gift" },
   ]
 
 }
