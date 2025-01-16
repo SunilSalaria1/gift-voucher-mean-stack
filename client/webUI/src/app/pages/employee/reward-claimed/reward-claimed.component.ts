ff1056e7143ecb3b3b251432a04e477aac3d6e39
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-16 17:28:56 +0530

    updates in reward-claimed and select-gift-voucher

--- client/webUI/src/app/pages/employee/reward-claimed/reward-claimed.component.ts
+++ client/webUI/src/app/pages/employee/reward-claimed/reward-claimed.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reward-claimed',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatCardModule],
+  imports: [MatIconModule, MatButtonModule, MatCardModule,RouterLink],
   templateUrl: './reward-claimed.component.html',
   styleUrl: './reward-claimed.component.css'
 })
