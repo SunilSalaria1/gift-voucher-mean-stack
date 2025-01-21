fdbdbcc31f8aa357c928699fd668ab61400f9885
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-21 11:06:35 +0530

    admin logout button

--- client/webUI/src/app/shared/header/header.component.ts
+++ client/webUI/src/app/shared/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButton,MatIcon,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  logOut(){
+  localStorage.removeItem('loginUser')
+  }
 }
