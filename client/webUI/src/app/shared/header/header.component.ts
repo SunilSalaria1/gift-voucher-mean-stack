7f2b0102d0e7a15fc0615781a02ed68916815e0f
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-29 18:19:45 +0530

    updated header

--- client/webUI/src/app/shared/header/header.component.ts
+++ client/webUI/src/app/shared/header/header.component.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatButton, MatButtonModule } from '@angular/material/button';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,RouterLink],
+  imports: [MatButtonModule, MatIconModule, RouterLink,CommonModule,MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  userData:any=localStorage.getItem('loginUser');
+  user:any=JSON.parse(this.userData);
 
-  logOut(){
-  localStorage.removeItem('loginUser')
+  logOut() {
+    localStorage.removeItem('loginUser')
   }
 }
