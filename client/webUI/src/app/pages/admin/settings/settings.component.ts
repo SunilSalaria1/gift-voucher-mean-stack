521372090a4d8fc2f83d4812d1751bd17edf3c77
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-29 15:23:53 +0530

    updated add admin form

--- client/webUI/src/app/pages/admin/settings/settings.component.ts
+++ client/webUI/src/app/pages/admin/settings/settings.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule ,],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  addAdminForm!: FormGroup;
+  constructor(
+      private formBuilder: FormBuilder
+    ) { }
 
+    ngOnInit(): void {
+      // form
+      this.addAdminForm = this.formBuilder.group({
+        adminName: ['', Validators.required],
+        adminKey: [
+          '',
+          [Validators.required],
+        ],
+        adminCode: ['', Validators.required],
+       
+      });
+    }
 }
