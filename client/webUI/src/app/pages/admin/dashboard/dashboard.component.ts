aa371e09bfa008171225c963ae3cd1b33e88a2b4
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-01-30 17:22:39 +0530

    updated pie chart

--- client/webUI/src/app/pages/admin/dashboard/dashboard.component.ts
+++ client/webUI/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -2,15 +2,65 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
-
+import { Color, LegendPosition, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatCardModule,MatIconModule,MatTabsModule],
+  imports: [MatCardModule,MatIconModule,MatTabsModule,NgxChartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  constructor() {}
+  
+  view: [number, number] = [700, 400];
+
+  single = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    },
+      {
+      "name": "UK",
+      "value": 6200000
+    }
+  ];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: LegendPosition = LegendPosition.Right; 
+
+  colorScheme: Color = {
+    name: 'custom',
+    selectable: true,
+    group:ScaleType.Ordinal,
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
+
+  constructor() {
+    Object.assign(this, {this:this.single })
+  }
+
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
