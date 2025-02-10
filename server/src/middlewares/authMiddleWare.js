ccd1f2cb07f9ad0fed97a16e8cb428de72e292e1
Aniket sharma <aniket.sharma@lpinfotech.com>
2025-02-10 10:24:02 +0530

    new changes auth MIddleware

--- server/src/middlewares/authMiddleWare.js
+++ server/src/middlewares/authMiddleWare.js
@@ -1,13 +1,33 @@
 const jwt = require('jsonwebtoken');
+const SECRET_KEY = "Aniwer32432@#^%&^#!%@&#%&%!@#!&%@#!&@2153"
 
-const authenticateToken = async (req, res, next) => {
+const authenticateToken = (req, res, next) => {
+    // 1. Get token from the Authorization header
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
+    // 2. Check if token is not present
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided. Invalid token.' });
+    }
 
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    // 3. Verify the token
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) {
+            if (err.name === 'TokenExpiredError') {
+                // If the token is expired, return the token expired error
+                return res.status(401).json({ message: 'Token expired' });
+            } else {
+                // If any other error (e.g., invalid token), return invalid token error
+                return res.status(403).json({ message: 'Invalid token' });
+            }
+        }
 
-    
+        // Attach the user information (decoded token) to the request object
+        req.user = user;
 
-    next()
-    //   })
-}
-module.exports = { authenticateToken }
+        // Call the next middleware or route handler
+        next();
+    });
+};
+
+module.exports = { authenticateToken };
