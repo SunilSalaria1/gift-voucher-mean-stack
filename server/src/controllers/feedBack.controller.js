69c91c9060e1ea1d5d73f6af4282203d2529e7a8
Aniket Sharma <aniket.sharma@lpinfotech.com>
2025-02-13 16:45:36 +0530

    get all feedbacks api

--- server/src/controllers/feedBack.controller.js
+++ server/src/controllers/feedBack.controller.js
@@ -29,5 +29,101 @@ const addFeedback = async (req, res) => {
     }
 };
 
+const getAllFeedbacks = async (req, res) => {
+    /*  #swagger.tags = ['Feedback section']
 
-module.exports = { addFeedback }
+               */
+    try {
+        await connectDB();
+        // const useriddata = await feedbackCollection.find().toArray()
+        const feedbacks = await feedbackCollection.aggregate([{
+            $addFields: {
+                userIdObj: {
+                    $cond: {
+                        if: { $eq: [{ $strLenCP: "$userId" }, 24] }, // Check if userId is 24 characters long
+                        then: { $toObjectId: "$userId" },  // Convert valid userId to ObjectId
+                        else: null  // Set null if invalid
+                    }
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "users",              // Join with users collection
+                localField: "userIdObj",           // userId in feedback collection
+                foreignField: "_id",            // _id in users collection
+                as: "userDetails"           // Store result in 'userDetails'
+            }
+        },
+        {
+            $unwind: "$userDetails"             // Convert userDetails array into object
+        },
+        {
+            $project: {
+                "userDetails.password": 0,
+                "userDetails.tokens": 0,
+                "userDetails.isDeleted": 0,
+                "userDetails.isAdmin": 0,
+                "userDetails.isPrimaryAdmin": 0
+            }
+        }
+        ]).toArray();
+        return res.status(200).json({ feedbacks })
+
+
+    } catch (e) {
+        console.error("Error fetching feedbacks:", e);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+// const getAllFeedbacks = async (req, res) => {
+//     try {
+//         await connectDB();
+//         const feedbacks = await feedbackCollection.aggregate([
+//             {
+//                 $addFields: {
+//                     userIdObj: {
+//                         $cond: {
+//                             if: { $eq: [{ $strLenCP: "$userId" }, 24] }, // Check if userId is 24 characters long
+//                             then: { $toObjectId: "$userId" },  // Convert valid userId to ObjectId
+//                             else: null  // Set null if invalid
+//                         }
+//                     }
+//                 }
+//             },
+//             {
+//                 $lookup: {
+//                     from: "users",
+//                     localField: "userIdObj",  // Use converted ObjectId
+//                     foreignField: "_id",
+//                     as: "userDetails"
+//                 }
+//             },
+//             {
+//                 $match: { "userDetails": { $ne: [] } }  // Remove feedbacks with no valid user match
+//             },
+//             {
+//                 $unwind: "$userDetails"
+//             },
+//             {
+//                 $project: {
+//                     "userDetails.password": 0,
+//                     "userDetails.tokens": 0,
+//                     "userDetails.isDeleted": 0,
+//                     "userDetails.isAdmin": 0,
+//                     "userDetails.isPrimaryAdmin": 0
+//                 }
+//             }
+//         ]).toArray();
+
+//         return res.status(200).json({ feedbacks });
+
+//     } catch (e) {
+//         console.error("Error fetching feedbacks:", e);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// };
+
+
+module.exports = { addFeedback, getAllFeedbacks }
--- server/src/routes/feedback.router.js
+++ server/src/routes/feedback.router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const feedbackRouter = express.Router();
-const { addFeedback } = require('../controllers/feedBack.controller')
+const { addFeedback, getAllFeedbacks } = require('../controllers/feedBack.controller')
 const { authenticateToken } = require("../middlewares/authMiddleWare")
 
 feedbackRouter.post('/addFeedback', addFeedback)
+feedbackRouter.get('/getFeedbacks', getAllFeedbacks)
 
 module.exports = feedbackRouter;
