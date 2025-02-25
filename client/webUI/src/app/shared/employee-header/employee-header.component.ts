b9cfbfe04177e5340ee88d289ddd447db28004fc
Aniket Sharma <aniket.sharma@lpinfotech.com>
2025-02-25 16:07:11 +0530

    changes in dropdown

--- client/webUI/src/app/shared/employee-header/employee-header.component.ts
+++ client/webUI/src/app/shared/employee-header/employee-header.component.ts
@@ -4,49 +4,67 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
 import { UsersService } from '../../services/users.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-employee-header',
   standalone: true,
-  imports: [MatIconModule,RouterLink,MatButtonModule,ReactiveFormsModule, MatDialogModule, MatFormFieldModule, CommonModule, MatInputModule],
+  imports: [
+    MatIconModule,
+    RouterLink,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    CommonModule,
+    MatInputModule,
+    MatMenuModule,
+  ],
   templateUrl: './employee-header.component.html',
-  styleUrl: './employee-header.component.css'
+  styleUrl: './employee-header.component.css',
 })
-export class EmployeeHeaderComponent { 
-  private _usersService = inject(UsersService)
-  constructor(private router: Router,private snackBar: MatSnackBar ,private formBuilder: FormBuilder ) {}
+export class EmployeeHeaderComponent {
+  private _usersService = inject(UsersService);
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private formBuilder: FormBuilder
+  ) {}
 
-  submitted: boolean = false;   
-    feedbackForm!: FormGroup;
-    @ViewChild('content') dialogTemplate!: TemplateRef<any>;
-    readonly dialog = inject(MatDialog);
-    loggedUser = JSON.parse(localStorage.getItem('loginUser') || '{}');
-  
+  submitted: boolean = false;
+  suggestionsForm!: FormGroup;
+  @ViewChild('content') dialogTemplate!: TemplateRef<any>;
+  readonly dialog = inject(MatDialog);
+  loggedUser = JSON.parse(localStorage.getItem('loginUser') || '{}');
 
   ngOnInit(): void {
-      // form
-      this.feedbackForm = this.formBuilder.group({
-        productDescription: [
-          '',
-          [Validators.required, Validators.maxLength(200)],
-        ],
-      });
-      console.log('Form Initialized:', this.feedbackForm);
-    }   
-   
-  
+    // form
+    this.suggestionsForm = this.formBuilder.group({
+      productDescription: [
+        '',
+        [Validators.required, Validators.maxLength(200)],
+      ],
+    });
+    console.log('Form Initialized:', this.suggestionsForm);
+  }
+
   isLoginPage(): boolean {
     return this.router.url === '/employee-code';
   }
 
   isSelectedGiftDetailsPage(): boolean {
     return this.router.url === '/selected-gift-details';
-  }  
+  }
 
   isSelectGiftPage(): boolean {
     return this.router.url === '/select-gift-voucher';
@@ -56,71 +74,71 @@ export class EmployeeHeaderComponent {
     return this.router.url === '/reward-claimed';
   }
 
-  isHomePage(){
+  isHomePage() {
     return this.router.url === '/home';
   }
 
-  isAdminLoginPage(){
+  isAdminLoginPage() {
     return this.router.url === '/admin-login-access';
   }
 
-  logOut(){
+  logOut() {
     this._usersService.logout();
-      //success snackbar
+    //success snackbar
     this.snackBar.open('You have successfully logged out.', 'close', {
       duration: 5000,
       panelClass: ['snackbar-success'],
-      horizontalPosition: "center",
-      verticalPosition: "top",
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
     });
   }
 
   // dialog
   openDialog(): void {
-
-    this.feedbackForm.reset();    
+    this.suggestionsForm.reset();
     this.submitted = false;
     // Use the TemplateRef for the dialog
     const dialogRef = this.dialog.open(this.dialogTemplate, {
       width: '1200px',
     });
 
-
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
 
-  
   // Method to handle Send button click (when form is valid)
   send(): void {
-    if (this.feedbackForm.valid) {
+    if (this.suggestionsForm.valid) {
       // add feedback
       let payload: any;
       payload = {
-        userId: this.loggedUser._id,      
-        description: this.feedbackForm.value.productDescription,
-      }
+        userId: this.loggedUser._id,
+        description: this.suggestionsForm.value.productDescription,
+      };
       this._usersService.addSuggestions(payload).subscribe(
         (response: any) => {
-          console.log(this.feedbackForm.value); // Log form values
-          this.dialog.closeAll(); // Close dialog 
-          console.log("add feedback successfull:", response)
-           //success snackbar
-    this.snackBar.open('You have successfully shared your feedback.', 'close', {
-      duration: 5000,
-      panelClass: ['snackbar-success'],
-      horizontalPosition: "center",
-      verticalPosition: "top",
-    });
+          console.log(this.suggestionsForm.value); // Log form values
+          this.dialog.closeAll(); // Close dialog
+          console.log('add feedback successfull:', response);
+          //success snackbar
+          this.snackBar.open(
+            'You have successfully shared your feedback.',
+            'close',
+            {
+              duration: 5000,
+              panelClass: ['snackbar-success'],
+              horizontalPosition: 'center',
+              verticalPosition: 'top',
+            }
+          );
         },
         (error) => {
-          console.error('error in adding feedback:', error)
+          console.error('error in adding feedback:', error);
         }
-      )
-
+      );
     } else {
-      // this.feedbackForm.markAllAsTouched(); // Trigger validation messages
+      // this.suggestionsForm.markAllAsTouched(); // Trigger validation messages
       this.submitted = true;
     }
   }
