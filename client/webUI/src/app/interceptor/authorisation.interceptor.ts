02253333d455ba0723e4fb3e18c9e9e427ee33e4
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-02-26 18:57:10 +0530

    update in dashboard

--- client/webUI/src/app/interceptor/authorisation.interceptor.ts
+++ client/webUI/src/app/interceptor/authorisation.interceptor.ts
@@ -3,8 +3,10 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
 import { tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const authorisationInterceptor: HttpInterceptorFn = (req, next) => {
+  const snackBar = inject(MatSnackBar);
   const userService = inject(UsersService);
   const router = inject(Router);
   const token = userService.getToken(); // Retrieve token
@@ -14,11 +16,16 @@ export const authorisationInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(clonedRequest).pipe(
     tap({
-      error: (error) => {
-        console.log('@@@@@')
+      error: (error) => {        
         if (error.status === 401) {
           userService.logout(); // Clear token and user data
           router.navigate(['/home']); // Redirect to home page
+          snackBar.open('Session expired. Please log in again.', 'close', {
+            duration: 5000,
+            panelClass: ['snackbar-success'],
+            horizontalPosition: "center",
+            verticalPosition: "top",
+          });
         }
       }
     })
