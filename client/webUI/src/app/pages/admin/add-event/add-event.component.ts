4b196f7adfeb33251ec881fcfda211690d56390d
Jyoti-thakur-lp <jyoti@lpinfotech.com>
2025-02-28 16:24:12 +0530

    update in add event

--- client/webUI/src/app/pages/admin/add-event/add-event.component.ts
+++ client/webUI/src/app/pages/admin/add-event/add-event.component.ts
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, RouterLink } from '@angular/router';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { ProductsService } from '../../../services/products.service';
 @Component({
   selector: 'app-add-event',
   standalone: true,
@@ -32,22 +33,56 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 })
 export class AddEventComponent {
   constructor(
-    private formBuilder: FormBuilder, private snackBar: MatSnackBar, private router: Router,
+    private formBuilder: FormBuilder, private snackBar: MatSnackBar, private router: Router,private productsService: ProductsService
   ) { }
   addEventForm!: FormGroup;
+  eventImage: any;
+  imageId: any; 
 
   ngOnInit(): void {
     // form
     this.addEventForm = this.formBuilder.group({
-      eventName: ['', [Validators.required, Validators.minLength(3)]],
-      eventDate: ['', Validators.required],
-      eventTime: ['', Validators.required],
-      eventImage: ['', Validators.required],
-      venueAddress: ['', Validators.required],
-      note: ['', Validators.required],
-      attend: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      image: ['', Validators.required],
+      address: ['', Validators.required],
+      note: '',
+      attend: '',
       about: ['', Validators.required],
     });
+  }
+
+
+  // selecting image
+  selectedFiles: any;
+  selectFiles(event: any): void {
+    this.selectedFiles = event.target.files[0];
+    if (this.selectedFiles) {
+      // Upload image when a file is selected
+      this.uploadImage(this.selectedFiles);
+    }
+  }
+
+  // image upload to api
+  uploadImage(file: File) {
+    const formData = new FormData()
+    formData.append('file', file, file.name)
+    this.productsService.uploadProductImage(formData).subscribe(
+      (response) => {
+        this.eventImage = response.imageUrl
+        this.imageId = response.fileDetails._id
+        this.addEventForm.patchValue({
+          productImage: response.fileDetails.fileName
+        })
+      }
+    )
+  }
+
+  // deleting image
+  removeImage() {
+    this.selectedFiles = '';
+    this.addEventForm.get('eventImage')?.setValue('');
   }
   submitForm(): void { 
   }
---
+++ client/webUI/src/app/services/events.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private apiUrl = 'http://localhost:3000';
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+
+  // add event
+  addEvent(postData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/event`, postData);
+  }
+
+  // get events
+  getEvents(page: number,
+    limit: number,
+    searchItem: any,
+    sortBy: string = ''): Observable<any> {
+    const params: any = {      
+      page: page,
+      limit: limit,
+    };
+    if (searchItem.searchTerm != '') {
+      console.log('222222222222');
+      params.searchItem = searchItem;
+    }
+    if (sortBy) {
+      params.sortBy = sortBy;
+    }
+    return this.http.get(`${this.apiUrl}/api/event`);
+  }
+}
